e9588f4727f2de1e777451f7a71dc741dde6a8ea
Giorgi Manjavidze <manjavidze007@gmail.com>
2025-06-04 22:23:32 +0400

    update

--- Task 2- Opaque and Boxed Protocol Types.playground/Contents.swift
+++ Task 2- Opaque and Boxed Protocol Types.playground/Contents.swift
@@ -5,30 +5,48 @@ protocol Shape {
 
 
 
-struct Circle: Shape {
+class Circle: Shape {
     var radius: Double
-
+    
+    init(radius: Double) {
+        self.radius = radius
+    }
+    
     func area() -> Double {
         return .pi * radius * radius
     }
-
+    
     func perimeter() -> Double {
         return 2 * .pi * radius
     }
+    
+    deinit {
+            print("Circle deallocated")
+        }
 }
 
-struct Rectangle: Shape {
+class Rectangle: Shape {
     var width: Double
     var height: Double
-
+    
+    init(width: Double, height: Double) {
+        self.width = width
+        self.height = height
+    }
+    
     func area() -> Double {
         return width * height
     }
-
+    
     func perimeter() -> Double {
         return 2 * (width + height)
     }
+    
+    deinit {
+            print("Recteangle deallocated")
+        }
 }
+
 
 func generateShape() -> some Shape {
     return Circle(radius: 5)
@@ -37,3 +55,11 @@ func generateShape() -> some Shape {
 func calculateShapeDetails(for shape: some Shape) -> (area: Double, perimeter: Double) {
     return (shape.area(), shape.perimeter())
 }
+
+
+do {
+    let shape = generateShape()
+    let result = calculateShapeDetails(for: shape)
+    print("Area: \(result.area)")
+    print("Perimeter: \(result.perimeter)")
+} 
